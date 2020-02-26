17296bb480b821b9b54fdae73877a50637b593d6
Daniel Siepmann <coding@daniel-siepmann.de>
2020-02-26 13:07:47 +0100

    INIT example with rss registration

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Dashboard Proof of Concept',
+    'description' => 'Proof of Concept for alternative Widget API.',
+    'category' => 'be',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Daniel Siepmann',
+    'author_email' => 'coding@daniel-siepmann.de',
+    'author_company' => '',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'core' => '',
+            'dashboard' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [
+        ],
+    ],
+];
